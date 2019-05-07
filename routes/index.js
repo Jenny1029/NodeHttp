a3dd20aaa15f55535274381ce428219eee4c8d8c
jenny <wb-kmz309432@alibaba-inc.com>
2019-05-07 14:26:20 +0800

    save

--- routes/index.js
+++ routes/index.js
@@ -9,8 +9,6 @@ var xlsx = require('node-xlsx');
 var fileDir = fs.readdirSync('./data');
 var zlib = require('zlib');
 var rp = require('request-promise');
-
-return;
 //console.log(fileDir);
 //var file = fs.readFileSync('./data')
 /* fileDir.map((fileItem, index) => {
@@ -36,7 +34,7 @@ while(i < auditers.length) {
 } */
 
 //httpRequestServerPromise(auditers[0], 1);
-httpRequestServer(auditers[0], 1);
+/* httpRequestServer(auditers[0], 1);
 
 function writeExcelFile() {
   console.log('哈哈')
@@ -59,7 +57,7 @@ function httpRequestServerPromise(auditer, page) {
   const options = {
     uri: `https://dayu.alibaba-inc.com/dayubg/defaultScreen.do?event_submit_do_Qry_All_Ord_For_Super_Admin=anything&action=viewAction&_input_charset=UTF-8&startDate=2014-01-01%2000%3A00%3A00&endDate=2018-07-30%2023%3A59%3A59&curPage=${page}&pageSize=10&auditerUser=${auditer}&ordLevel=IMPORTANT&auditStates=1&custFrom=1`,
     headers: {
-      'Accept':'*/*',
+      'Accept':'*',
       'Accept-Encoding':'utf-8',  //这里设置返回的编码方式 设置其他的会是乱码
       'Accept-Language':'zh-CN,zh;q=0.8',
       Cookie: 'UM_distinctid=164f501aaa822e-02d78c4519788e-163f6952-13c680-164f501aaa9e70; dayu-bg-web_USER_COOKIE=82B17C000765EEB9BB7E0DF9FDF45C414CB82113D65212035A14D53ACE0300D7A5989BCCAAA4C07EAAD477A92ABCC63D0722537C8DE33997A7300E5F1A48F167F277B192F3B6D3D7329DDC1D3D2ACCD6A2D6BC00352ABC5BC72FBA4144D0A8D3383D8F9C9DF156B6527FE1CA7EDACB035E797754559FD8C5AC9E287C10D4C3DD343AD4E8A751042A18D9B54B5C14B8963DB071969331F72435E2937BA1C05D3EC388718CD170202F2ABFC6DE6C90ACB1; cna=nmboEzPNS1QCAWoLIgW2OQvm; fusion-role=developer; fusion-locale=zh_CN; fusion-bu=51; fusion-pkg=next; fusion-next-base=0; JSESSIONID=96CE1D74E0D50C41A23EDCF851F173AF; dayu-bg-web_SSO_TOKEN=B09F8440F22B456D4C1A674A1DA56EF391C085A9148073838C5D78122C20D0DD1ED252EB03BFBC83C06A882CDCADE75E; dayu-bg-web_LAST_HEART_BEAT_TIME=2D9B629CB466BB332B30D4B0273C6D8E; isg=BBsbLD-fxMpG6jgPeTV7wVJTqnlFWC6UI75kJg1YZJox7D7OlcJVQoAqgw5Hb4fq',
@@ -82,7 +80,7 @@ function httpRequestServer(auditer, page) {
     port:80,
     method: 'GET',
     headers: {
-      'Accept':'*/*',
+      //'Accept':'*',
       'Accept-Encoding':'utf-8',  //这里设置返回的编码方式 设置其他的会是乱码
       'Accept-Language':'zh-CN,zh;q=0.8',
       Cookie: 'UM_distinctid=164f501aaa822e-02d78c4519788e-163f6952-13c680-164f501aaa9e70; dayu-bg-web_USER_COOKIE=82B17C000765EEB9BB7E0DF9FDF45C414CB82113D65212035A14D53ACE0300D7A5989BCCAAA4C07EAAD477A92ABCC63D0722537C8DE33997A7300E5F1A48F167F277B192F3B6D3D7329DDC1D3D2ACCD6A2D6BC00352ABC5BC72FBA4144D0A8D3383D8F9C9DF156B6527FE1CA7EDACB035E797754559FD8C5AC9E287C10D4C3DD343AD4E8A751042A18D9B54B5C14B8963DB071969331F72435E2937BA1C05D3EC388718CD170202F2ABFC6DE6C90ACB1; cna=nmboEzPNS1QCAWoLIgW2OQvm; fusion-role=developer; fusion-locale=zh_CN; fusion-bu=51; fusion-pkg=next; fusion-next-base=0; JSESSIONID=96CE1D74E0D50C41A23EDCF851F173AF; dayu-bg-web_SSO_TOKEN=B09F8440F22B456D4C1A674A1DA56EF391C085A9148073838C5D78122C20D0DD1ED252EB03BFBC83C06A882CDCADE75E; dayu-bg-web_LAST_HEART_BEAT_TIME=2D9B629CB466BB332B30D4B0273C6D8E; isg=BK2tfd0FimS7Hm6529_Ff1BZvE_nouC6YYTSMO-yDMSzZsUYt1gIrE5wVXglR_mU',
@@ -97,13 +95,13 @@ function httpRequestServer(auditer, page) {
     //res.header("Content-Type", "application/json;charset=utf-8");
     var header = res.headers;
     //console.log(header);
-    /* if(res.headers['content-encoding']=='gzip'){
-      var gzip=zlib.createGunzip();
-      res.pipe(gzip);
-      output=gzip;
-    }else{
-        output=res;
-    } */
+    //if(res.headers['content-encoding']=='gzip'){
+     //var gzip=zlib.createGunzip();
+    //res.pipe(gzip);
+     // output=gzip;
+    //}else{
+      //  output=res;
+    } 
     res.on('data', (chunk) => {
        response = chunk.toString('utf-8');
     });
@@ -129,8 +127,118 @@ function httpRequestServer(auditer, page) {
   req.end((res) => {
     console.log('request======>>>105',res);
   });
+} */
+
+/* var fs = require('fs');
+
+function readLines(input, func) {
+  var remaining = '';
+
+  input.on('data', function(data) {
+    remaining += data;
+    var index = remaining.indexOf('\n');
+    while (index > -1) {
+      var line = remaining.substring(0, index);
+      remaining = remaining.substring(index + 1);
+      func(line);
+      index = remaining.indexOf('\n');
+    }
+  });
+
+  input.on('end', function() {
+    if (remaining.length > 0) {
+      func(remaining);
+    }
+  });
 }
-    
+
+function func(data) {
+  console.log(`insert into qn_corp_number(taobao_uid,  ram_id,  real_name_ins_id,  number,  region_name_province,  region_name_city,  corp_name,  monthly_price, spec_name) values(0, '1588024538762430', 100000025474040, '${data}', '广东2', '深圳', '客服电话', '35', '广州电信_GZCT');`);
+}
+
+var remaining = '';
+var numberArray = [];
+fs.readFile('routes/number.txt','utf-8', (err, data) => {
+  // const data1 = new Buffer(data, 'utf-8');
+  console.log('data=====', data);
+  remaining += data;
+  var index = remaining.indexOf('\n');
+  while (index > -1) {
+    var line = remaining.substring(0, index);
+    remaining = remaining.substring(index + 1);
+    func(line);
+    index = remaining.indexOf('\n');
+  }
+  console.log(remaining);
+}); */
+//readLines(input, func);
+
+/* 
+var xlsx = require('node-xlsx');
+var fs = require('fs');
+//读取文件内容
+var obj = xlsx.parse('./data/skillGroups.xlsx');
+var excelObj=obj[0].data;
+const totalLength = Object.keys(excelObj).length;
+console.log(totalLength);
+
+realCount = (val) => {
+  let realLength = 0;
+  let charCode = -1;
+  for (let i = 0; i < val.length; i++) {
+    charCode = val.charCodeAt(i);
+    if (charCode >= 0 && charCode <= 128) {
+      realLength += 1;
+    } else {
+      realLength += 2;
+    }
+  }
+  return realLength;
+}
+
+var data = [];
+let arrLength = {};
+let skillPercent = {};
+for(const index in excelObj){
+    var value=excelObj[index];
+    if (Object.keys(arrLength).includes(String(realCount(value[0])))) {
+      arrLength[realCount(value[0])]  = arrLength[realCount(value[0])] += 1;
+    } else {
+      arrLength[realCount(value[0])] = 1;
+    }
+}
+for(var key in arrLength) {
+  skillPercent[key] = `${((arrLength[key] / totalLength) * 100).toFixed(2)}%`;
+}
+console.log(arrLength, skillPercent); */
+/* var buffer = xlsx.build([
+    {
+        name:'sheet1',
+        data:arrLength
+    }        
+]);
+
+//将文件内容插入新的文件中
+fs.writeFileSync('test1.xlsx',buffer,{'flag':'w'}); */
+let sql = '';
+for(var loop = 0; loop <=3; loop++) {
+  let beginIndex = 80334095 + 2000*loop;
+  console.log(beginIndex);
+  let sql = '';
+  for(var item=1; item <=2000; item++) {
+    let beginNum = beginIndex + item;
+    simpleSql = `(${beginNum}, 1, 0, now(), now()),\n`;
+    if(item == 2000) {
+      simpleSql = `(${beginNum}, 1, 0, now(), now());\n`;
+    }
+    sql += simpleSql;
+  }
+  var dataText = `insert into acc_extension(extension, cuslabelid, state, createtime, updatetime) values ${sql}`;
+  fs.writeFileSync(`sql${loop+2}.txt`, dataText, {'flag':'w'});
+}
+
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
